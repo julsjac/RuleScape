9194cd46f403cafb251d549bd9a6a93a81ea9b28
Electra <electra@bu.edu>
2026-04-21 20:34:43 -0400

    Update random_forest.py
    Adding top features extraction

--- dataio/ml/models/random_forest.py
+++ dataio/ml/models/random_forest.py
@@ -2,13 +2,19 @@ from sklearn.ensemble import RandomForestClassifier
 from sklearn.metrics import accuracy_score
 
 
-def train(X_train, y_train, X_test, y_test, max_depth=3):
+def train(X_train, y_train, X_test, y_test, rules_N, max_depth=3):
     model = RandomForestClassifier(max_depth=max_depth, random_state=0)
     model.fit(X_train, y_train)
 
     preds = model.predict(X_test)
+    
+    # 2. Extract importances and pair with feature names
+    importances = pd.Series(model.feature_importances_, index=feature_names)
+    # 3. Sort and get the top 10
+    top_features = importances.sort_values(ascending=False).head(rules_N)
 
     return {
         "model": model,
         "accuracy": accuracy_score(y_test, preds)
+        "top_n_rules": top_features
     }
